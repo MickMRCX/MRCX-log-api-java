3eef432e2faa2c496b812fe1a2b3710650914445
Pascal Marcouyoux <pmarcou@outlook.com>
2016-07-11 17:22:26 +0200

    DatabaseAccess

---
+++ src/dataAccessLayer/DataBaseAccess.java
@@ -0,0 +1,35 @@
+package dataAccessLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+
+import businessLogicLayer.Log;
+import constants.Severity;
+
+public class DataBaseAccess {
+	
+	public static Connection getConnection(){
+		Connection cnx=null;
+		try {
+			Class.forName("YOUR SQL DRIVER CLASS NAME");
+			System.out.println("Driver O.K.");
+			
+			String url = "jdbc:DATABASE://DATABASE_ADDRESS";
+			String user = "USERNAME";
+			String passwd = "PASSWORD";
+			
+			cnx = DriverManager.getConnection(url, user, passwd);
+			
+		} catch (SQLException e) {
+			Log log = new Log(e, Severity.Critical, new Date(), "Could not connect to database!");
+			//TODO Ecrire dans rapport.txt
+		} catch (Exception e){
+			Log log = new Log(e, Severity.Critical, new Date(), "An exception occured while trying to connect to the database!");
+			//TODO Ecrire dans rapport.txt
+		}
+		
+		return cnx;
+	}
+}
