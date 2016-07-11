2a44c956f82118efbfa13a1d4ae740e096d4fbd1
Pascal Marcouyoux <tidus56fr@hotmail.fr>
2016-07-11 11:42:50 +0200

    log business object

--- src/bo/Log.java
+++ src/bo/Log.java
@@ -3,11 +3,68 @@
  */
 package bo;
 
+import java.io.Serializable;
+import java.util.Date;
+import constants.Severity;
+
 /**
  * @author Pascal Marcouyoux
  * @url https://github.com/P-marcou
  *
  */
-public class Log {
+public class Log implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int identifier;
+	private Exception exception;
+	private Severity severity;
+	private Date date;
+	private String message;
+	
+	
+	
+	
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(int identifier) {
+		this.identifier = identifier;
+	}
+
+	public Exception getException() {
+		return exception;
+	}
+	
+	public void setException(Exception exception) {
+		this.exception = exception;
+	}
+	
+	public Severity getSeverity() {
+		return severity;
+	}
+	
+	public void setSeverity(Severity severity) {
+		this.severity = severity;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
