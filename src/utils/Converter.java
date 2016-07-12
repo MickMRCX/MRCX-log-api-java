e31cc3f5cf55c1558369440cf3eef09f4d876968
Pascal Marcouyoux <pmarcou@outlook.com>
2016-07-12 15:21:35 +0200

    Converter

---
+++ src/utils/Converter.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Converter {
+	
+	/**
+	 * Convert an Exception stacktrace to a String writable in DB
+	 * @param e
+	 * @return the exception's stacktrace
+	 */
+	public static String StacktraceToString(Exception e){
+		String returned = null;
+		
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		
+		e.printStackTrace(pw);
+		
+		returned = sw.toString();
+		
+		
+		try {
+			pw.close();
+			sw.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return returned;
+		
+	}
+
+}
