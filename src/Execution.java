cf7fbfb7877ad0893d2d505bca9d35be72de5e97
Pascal Marcouyoux <pmarcou@outlook.com>
2016-07-18 14:00:06 +0200

    Sample of writing in text file

---
+++ src/Execution.java
@@ -0,0 +1,63 @@
+import java.util.Date;
+
+import businessLogicLayer.Log;
+import constants.Severity;
+import dataAccessLayer.LogTextFile;
+
+/**
+ * 
+ */
+
+/**
+ * @author pascal
+ *
+ */
+public class Execution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		LogTextFile rapport = LogTextFile.getInstance( );
+
+		rapport.initialize("/home/pascal/Musique/", "rapport.txt", "Bonjour ceci est un test");
+
+		if(rapport.createFile( )){
+
+			Log log = new Log(Severity.Verbose, new Date( ), "Never gonna");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Information, new Date( ), "give you up");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Warning, new Date( ), "let you down");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Error, new Date( ), "run around and desert you");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Critical, new Date( ), "make you cry");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Warning, new Date( ), "say goodbye");
+
+			rapport.writeLog(log);
+
+			log = new Log(Severity.Error, new Date( ), "tell a lie and hurt you");
+
+			rapport.writeLog(log);
+
+			rapport.writeSeparation( );
+
+		}else{
+			System.out.println("Le fichier n'existe pas et n'a pas pu être créé.");
+		}
+	}
+
+}
